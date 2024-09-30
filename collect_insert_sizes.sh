698bc9b5758616d968e9f19ac14033e193599c1c
PamAlamilla <166521967+PamAlamilla@users.noreply.github.com>
2024-09-30 15:58:48 -0400

    Update collect_insert_sizes.sh

--- collect_insert_sizes.sh
+++ collect_insert_sizes.sh
@@ -9,7 +9,7 @@
 #SBATCH --array=0-32 #Change to the number of BAM files minus 1
 
 #Description: This script reads through a list of BAM files and applies the CollectInsertSizeMetrics tool (from Picard) to each one. 
-#The output is two files, a text file containing insert size metrics and a pdf file with the associated histogram. 
+#The output is two files, a text file containing insert size metrics and a PDF file with the associated histogram. 
 #Author: Pamela Alamilla
 #Date: 05/06/2024
 
@@ -18,14 +18,14 @@ module load R/4.2.1
 module load picard/2.6.0
 
 #Provide path to text file of BAM file names
-FILENAMES=/path/to/textfile.txt #Change this to your file with the BAM file names
+FILENAMES=/path/to/file.txt #Change this to your file with the BAM file names
 
 #Provide path to BAM files
-BAM_PATH=/path/to/bamfiles #Change this to the directory holding your BAM files
+BAM_PATH=/path/to/directory #Change this to the directory holding your BAM files
 
 #Provide paths to directories in which to deposit output files
-METRICS_PATH=/path/to/insert_size_metrics_directory #Change this your previously-made output directory 'insert_size_metrics'
-HISTOGRAMS_PATH=/path/to/insert_size_histograms_directory #Change this your previously-made output directory 'insert_size_histograms'
+METRICS_PATH=/path/to/directory #Change this to the directory that will hold the output text files
+HISTOGRAMS_PATH=/path/to/directory #Change this to the directory that will hold the output PDFs
 
 #Retrieve next BAM file name from provided text file
 NAMES=($(cat $FILENAMES))
