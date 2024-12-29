8cc209c3377c0009c22037fa1f712ca79a62fc60
PamAlamilla <166521967+PamAlamilla@users.noreply.github.com>
2024-12-28 22:56:59 -0500

    Update DELFI_analysis.R

--- DELFI_analysis.R
+++ DELFI_analysis.R
@@ -1,7 +1,7 @@
 setwd("/Users/pamalamilla/Desktop/Fragmentomics_GOOD/DELFI/")
 
 #Combine Bins----
-#Lifted  and adapted from GitHub: cancer-genomics/delfi_scripts/03.5-combine_bins.r
+#Lifted and adapted from GitHub: cancer-genomics/delfi_scripts/03.5-combine_bins.r
 library(tidyverse)
 library(GenomicRanges)
 library(multidplyr)
@@ -10,7 +10,7 @@ library(cowplot)
 
 bindir <- "~/Desktop/Fragmentomics_GOOD/DELFI/fragment_profiles_RDS" #Provide directory of RDS files
 
-metadata <- read.table("~/Desktop/Fragmentomics_GOOD/DELFI/metadata_discovery.txt") #Read in metadata table
+metadata <- read.table("~/Desktop/Fragmentomics_GOOD/metadata_discovery.txt") #Read in metadata table
 metadata <- metadata[2:144,] #Exclude sample LIB_15_0036_T1
 
 metadata_clean <- metadata %>% select(SAMPLE_ID, TYPE, DIAGNOSIS_CLASS)
@@ -126,8 +126,7 @@ summary.df$`TYPE` = relevel(as.factor(summary.df$`TYPE`), "CONTROL")
 
 saveRDS(summary.df, "./summary_tibble.rds")
 
-#Main Profile Plot----
-
+#Plot Genome-Wide Fragmentation Profiles----
 df.fr3 <- readRDS("./bins_5mbcompartments.rds")
 
 df.fr3 <- df.fr3 %>% 
@@ -136,7 +135,7 @@ df.fr3 <- df.fr3 %>%
 
 mytheme <- theme_classic(base_size=12) + theme(axis.text.x = element_blank(),
                                                axis.ticks.x=element_blank(),
-                                               #strip.text.y = element_blank(),
+                                               #strip.text.y = element_blank(), #Uncomment to remove labels along right y-axis
                                                strip.text.x = element_text(face="bold", size=20),
                                                strip.text.y = element_text(face="bold", size=20),
                                                axis.title.x = element_text(face="bold", size=50),
@@ -161,76 +160,71 @@ small.arms <- setNames(c("", "12q", "", "16q", "", "17q", "", "18q","", "19",
 arm.labels <- setNames(arm$arm, arm$arm)
 arm.labels[names(small.arms)] <- small.arms
 
-ggplot(df.fr3, aes(x=bin, y=ratio.centered, group=id)) +
+df.fr3$DIAGNOSIS_CLASS <- factor(df.fr3$DIAGNOSIS_CLASS)
+
+subtype = c(CONTROL = "CONTROL", FL = "FL", DLBCL = "DLBCL", HL = "HL")
+
+##Controls vs Lymphomas----
+gg = ggplot(df.fr3, aes(x=bin, y=ratio.centered, group=id)) +
   geom_line(color="gray50", size=1) +
-  #stat_summary(aes(group = type), geom = "line", fun.y = median, size = 1.5, color="steelblue")
   labs(x="", y="Fragmentation profile", color="") +
   facet_grid(TYPE~arm, switch="x",space="free_x", scales="free_x", labeller=labeller(type=tissue, arm=arm.labels)) + 
   coord_cartesian(xlim = NULL, ylim=c(-.10,.12), expand = TRUE) +
   mytheme
 
-#save_plot("./tmp.pdf", gg, ncol=1, nrow=1, base_height=12, base_width=35)
+save_plot("../Figures/DELFI_fragment_profiles_2groups.pdf", gg, ncol=1, nrow=1, base_height=12, base_width=35)
 
-control.summary.tib = summary.df %>% filter(TYPE=="CONTROL") %>% select(ratio2.corrected.cor)
-lymphoma.summary.tib = summary.df %>% filter(TYPE=="LYMPHOMA") %>% select(ratio2.corrected.cor)
-
-control.summary.ratio2corrected = control.summary.tib[,2] %>% unlist()
-lymphoma.summary.ratio2corrected = lymphoma.summary.tib[,2] %>% unlist() 
-
-control.summary.ratio2corrected %>% unlist() %>% median() #0.860458 median correlation
-lymphoma.summary.ratio2corrected %>% unlist() %>% median() #0.7616431 median correlation
-
-wilcox.test(control.summary.ratio2corrected, lymphoma.summary.ratio2corrected) #p-value = 1.434e-05
-
-boxplot(control.summary.ratio2corrected, lymphoma.summary.ratio2corrected, 
-        ylim = c(0,1), names = c("Control", "Lymphoma"))
-
-
-#BY SUBTYPE----
-##Plot----
-df.fr3$DIAGNOSIS_CLASS <- factor(df.fr3$DIAGNOSIS_CLASS)
-
-
-subtype = c(CONTROL = "CONTROL", FL = "FL", DLBCL = "DLBCL", HL = "HL")
-
-ggplot(df.fr3, aes(x=bin, y=ratio.centered, group=id)) +
+##By Subtype----
+gg2 = ggplot(df.fr3, aes(x=bin, y=ratio.centered, group=id)) +
   geom_line(color="gray50", size=1) +
   labs(x="", y="Fragmentation profile", color="") +
-  facet_grid(DIAGNOSIS_CLASS~arm, 
-             switch="x",
-             space="free_x", 
-             scales="free_x", 
+  facet_grid(DIAGNOSIS_CLASS~arm, switch="x", space="free_x", scales="free_x", 
              labeller=labeller(DIAGNOSIS_CLASS = subtype, arm=arm.labels)) + 
   coord_cartesian(xlim = NULL, ylim=c(-.10,.12), expand = TRUE) +
   mytheme
 
-##Summary----
+save_plot("../Figures/DELFI_fragment_profiles_4groups.pdf", gg2, ncol=1, nrow=1, base_height=24, base_width=35)
+
+#Calculate Profile Correlations----
 summary.df.2 = inner_join(summary.df, metadata_clean[,c(1,3)], by=c("id"="SAMPLE_ID")) #Add DIAGNOSIS_CLASS column
 
-control.summary.tib = summary.df.2 %>% filter(DIAGNOSIS_CLASS=="CONTROL") %>% select(ratio2.corrected.cor)
+control.summary.tib = summary.df %>% filter(TYPE=="CONTROL") %>% select(ratio2.corrected.cor)
+lymphoma.summary.tib = summary.df %>% filter(TYPE=="LYMPHOMA") %>% select(ratio2.corrected.cor)
 DLBCL.summary.tib = summary.df.2 %>% filter(DIAGNOSIS_CLASS=="DLBCL") %>% select(ratio2.corrected.cor)
 FL.summary.tib = summary.df.2 %>% filter(DIAGNOSIS_CLASS=="FL") %>% select(ratio2.corrected.cor)
 HL.summary.tib = summary.df.2 %>% filter(DIAGNOSIS_CLASS=="HL") %>% select(ratio2.corrected.cor)
 
 control.summary.ratio2corrected = control.summary.tib[,2] %>% unlist()
+lymphoma.summary.ratio2corrected = lymphoma.summary.tib[,2] %>% unlist() 
 DLBCL.summary.ratio2corrected = DLBCL.summary.tib[,2] %>% unlist() 
 FL.summary.ratio2corrected = FL.summary.tib[,2] %>% unlist() 
 HL.summary.ratio2corrected = HL.summary.tib[,2] %>% unlist() 
 
 control.summary.ratio2corrected %>% unlist() %>% median() #0.860458 median correlation
+lymphoma.summary.ratio2corrected %>% unlist() %>% median() #0.7616431 median correlation
 DLBCL.summary.ratio2corrected %>% unlist() %>% median() #0.7289043 median correlation
 FL.summary.ratio2corrected %>% unlist() %>% median() #0.7794597 median correlation
 HL.summary.ratio2corrected %>% unlist() %>% median() #0.7665276 median correlation
 
+wilcox.test(control.summary.ratio2corrected, lymphoma.summary.ratio2corrected) #p-value = 1.434e-05
 wilcox.test(control.summary.ratio2corrected, DLBCL.summary.ratio2corrected) #p-value = 4.867e-05
 wilcox.test(control.summary.ratio2corrected, FL.summary.ratio2corrected) #p-value = 0.004173
 wilcox.test(control.summary.ratio2corrected, HL.summary.ratio2corrected) #p-value = 0.0001001
 
+
+#Plot Corrected Ratios----
+##Controls vs Lymphomas----
+boxplot(control.summary.ratio2corrected, lymphoma.summary.ratio2corrected, 
+        ylim = c(0,1), names = c("Control", "Lymphoma"), 
+        main="GC-Corrected Ratios")
+
+##By Subtype----
 boxplot(control.summary.ratio2corrected, DLBCL.summary.ratio2corrected, 
         FL.summary.ratio2corrected, HL.summary.ratio2corrected,
-        ylim = c(0,1), names = c("Control", "DLBCL", "FL", "HL"))
+        ylim = c(0,1), names = c("Control", "DLBCL", "FL", "HL"),
+        main="GC-Corrected Ratios")
 
-
+DELFI_correctedRatios_4groups
 #Dataframe for ML----
 df_Victoria = df.fr3 %>% select(id, TYPE, DIAGNOSIS_CLASS, bin, 
                   short.corrected2, long.corrected2) %>%
